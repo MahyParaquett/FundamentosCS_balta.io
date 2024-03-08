a0cd10e9cac87538a4bf6f3c9c236ca88bd3dabd
MahyParaquett <mahyaraparaquett@gmail.com>
2024-03-08 16:31:01 -0300

    feat: moedas

---
+++ Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10548315.25m;
+
+            //Arredondar o valor
+            Math.Round(valor);
+
+            //Arredonda pra cima
+            Math.Ceiling(valor);
+
+            //Arredondar para baixo
+            Math.Floor(valor);
+        }
+    }
+}
