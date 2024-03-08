8b63f2a885c923cf30015c9048b43173184289f5
MahyParaquett <mahyaraparaquett@gmail.com>
2024-03-08 11:15:52 -0300

    feat: add funções ao menu

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -8,6 +8,10 @@ namespace EditorHtml
             Console.ForegroundColor = ConsoleColor.Black;
 
             DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine() ?? "0");
+            HandleMenuOption(option);
         }
 
         public static void DrawScreen(){
@@ -38,6 +42,41 @@ namespace EditorHtml
                             Console.Write("\n");
             }
         }
+
+        public static void WriteOptions(){
+            Console.SetCursorPosition(3,2); //move o cursor para linha 2 coluna 3 
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("================");
+            Console.SetCursorPosition(3,4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3,6);
+            Console.WriteLine("1- Novo arquivo");
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2- Abrir");
+            Console.SetCursorPosition(3,9);
+            Console.WriteLine("0- Sair");
+            Console.SetCursorPosition(3,10);
+            Console.WriteLine("Opção: ");
+            Console.SetCursorPosition(9,10);
+        }
+
+        public static void HandleMenuOption(short option){
+
+            switch(option){
+                case 1 : Editor.Show(); break;
+                case 2 : Console.WriteLine("View"); break;
+                case 0 : {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: Show();break;
+            }
+        }
+
+        
+
 
     } 
 
