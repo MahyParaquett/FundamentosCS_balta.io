4619b00fe3663fa146669a96c880bde2b674c374
MahyParaquett <mahyaraparaquett@gmail.com>
2024-03-08 12:31:16 -0300

    feat: editor html

--- EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
+++ EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a67435eddcf2097a4afdaaa69a07e38c2f8aae0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b63f2a885c923cf30015c9048b43173184289f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
